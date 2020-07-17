ffc9e0494e1d091411ba38e712b13d65bb74d68d
Gustavo Lima <gustavomdcl@gmail.com>
2020-07-17 18:17:14 -0300

    Challange accepted and created

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -15,14 +21,33 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const income = this.transactions.reduce(function(total,next){
+      return (next.type==='income')
+        ?total+next.value
+        :total;
+    },0);
+    const outcome = this.transactions.reduce(function(total,next){
+      return (next.type==='outcome')
+        ?total+next.value
+        :total;
+    },0);
+    const total = income - outcome;
+
+    const balance = {income,outcome,total};
+
+    return balance;
   }
 
-  public create(): Transaction {
+  public create({title,value,type}:CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({title,value,type});
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
